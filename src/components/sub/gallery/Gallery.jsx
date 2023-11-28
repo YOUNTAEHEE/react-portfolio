7382b41770b326e8cde55ccc1056639cfc8d69f6
YOUNTAEHEE <f_hyang@naver.com>
2023-11-28 09:18:36 +0900

    23.11.28

---
+++ src/components/sub/gallery/Gallery.jsx
@@ -0,0 +1,10 @@
+import './Gallery.scss';
+import Layout from '../../common/layout/Layout';
+
+export default function Gallery() {
+	return (
+		<Layout title={'Gallery'}>
+			<p>Gallery전용 컨텐츠</p>
+		</Layout>
+	);
+}
